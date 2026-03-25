416bff0095d7ed0a0074736deacbac98e6bbf3ac
Sihan Udayaratna <it24103532@my.sliit.lk>
2026-03-26 03:03:26 +0530

    ModuleNotFounfError preventing node/client lauch

--- client/client.py
+++ client/client.py
@@ -20,6 +20,9 @@ CURRENT_FILE = Path(__file__).resolve()
 PROJECT_ROOT = CURRENT_FILE.parent.parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.append(str(PROJECT_ROOT))
+PROTO_DIR = PROJECT_ROOT / "proto"
+if str(PROTO_DIR) not in sys.path:
+    sys.path.append(str(PROTO_DIR))
 
 from proto import hivechat_pb2, hivechat_pb2_grpc
 
